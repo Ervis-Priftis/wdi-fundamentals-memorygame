b8c17c7b62833237dcf13c39d848400371b6ba7a
Ervis <steliospriftis@gmail.com>
2016-09-12 14:49:20 +0300

    Game Logic (if,else) statement added

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,4 +2,9 @@ var cardOne = "queen";
 var	cardTwo = "queen";
 var cardThree = "king";
 var	cardFour = "king";
-console.log("JS file is connected to HTML! Woo!")
+
+if (cardTwo === cardFour) {
+	alert('You found a match!');
+}else {
+	alert('Sorry,try again.');
+}
