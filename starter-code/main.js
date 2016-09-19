1adfa1fb84d346c25e3fe9ce1b53e9dd368b7629
Ervis <steliospriftis@gmail.com>
2016-09-19 02:53:44 +0100

    Game Memory Final Project Done

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,63 @@
-//var cardOne = "queen";
-//var	cardTwo = "queen";
-//var cardThree = "king";
-//var	cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-/*if (cardTwo === cardFour) {
-	alert('You found a match!');
-}else {
-	alert('Sorry,try again.');
-}*/
+var cardsInPlay = [];
+
+
+
 var board = document.getElementById('game-board');
 
-function createBoard () {
-	for (var i = 0; i < cards. length; i++) {
-		var cardElement = document.createElement('div');
+function createBoard() {
 
-		cardElement.className = 'card';
+  for (var i=0; i<cards.length; i++) {
 
-		board.appendChild(cardElement);
-	}
+    var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';
+
+
+
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
+
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
 }
+
+
+
+function isTwoCards() {
+
+
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://pixabay.com/get/e13db50e21f01c22d2524518a33219c8b66ae3d11eb5134592f4c570/clubs-884194_1280.png'>"; //King
+	} else {
+		this.innerHTML = "<img src='https://pixabay.com/get/e13db50e21fc1c22d2524518a33219c8b66ae3d11eb5134592f3c87d/clubs-884198_1280.png'>"; //Queen
+	}
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
